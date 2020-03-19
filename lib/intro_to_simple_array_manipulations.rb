8d928348a21056e518b9a8bd3f520e7f18fa4182
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-03-19 19:44:57 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array, string)
+  array.pop(string)  
+end
+
+def pop_with_args(array)
+  array = array[0].pop
+end
